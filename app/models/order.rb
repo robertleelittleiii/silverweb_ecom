c1214d98819c40062e94d6a0636e7d00ac529836
Robert Lee Little III <rob@silverwebsystems.com>
2015-10-03 01:53:08 -0400

    Updated validation methods to rails 4 way.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,23 @@
+class ValidateCard < ActiveModel::Validator
+  def validate(record)
+    unless record.credit_card.valid?
+      record.credit_card.errors.full_messages.each do |message|
+        record.errors.add_to_base message
+      end
+    end
+  end
+    
+
+  private
+  def some_complex_logic
+    # ...
+  end
+end
+
+
+
+
+
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_items , dependent: :destroy
@@ -13,7 +33,7 @@ class Order < ActiveRecord::Base
     :credit_card_type, :cc_number, :cc_expires, :cc_verification
   
   # validate_on_create :validate_card
-   validates_with validate_card, on: :create
+  validates_with ValidateCard, on: :create
   
   composed_of :cc_expires, class_name: "DateTime",
     mapping: %w(Time to_s),
@@ -156,121 +176,117 @@ class Order < ActiveRecord::Base
         #    :login => Settings.gateway_login,
         #    :ticket => Settings.gateway_password,
         #    :test=>false
-      # )
-    else
-      puts("Paypal Gateway Activated")
-      gateway = ActiveMerchant::Billing::PaypalGateway.new(
-        login: Settings.gateway_login,
+        # )
+      else
+        puts("Paypal Gateway Activated")
+        gateway = ActiveMerchant::Billing::PaypalGateway.new(
+          login: Settings.gateway_login,
           password: Settings.gateway_password,
           signature: Settings.gateway_signature
           
-      )
-    end
-          puts("user.name #{user.name}")
+        )
+      end
+      puts("user.name #{user.name}")
 
-    response = gateway.purchase(price_in_cents(@cart), credit_card, purchase_options)
-  end
+      response = gateway.purchase(price_in_cents(@cart), credit_card, purchase_options)
+    end
     
-  transactions.create!(action: "purchase", amount: price_in_cents(@cart), response: response)
-  if not response.success? then
-    errors.add_to_base(response.message)
-  end
+    transactions.create!(action: "purchase", amount: price_in_cents(@cart), response: response)
+    if not response.success? then
+      errors.add_to_base(response.message)
+    end
    
-  # cart.update_attribute(:purchased_at, Time.now) if response.success?
+    # cart.update_attribute(:purchased_at, Time.now) if response.success?
     
     
     
-  response.success?
-end
-  
-def price_in_cents(cart)
-  (cart.grand_total_price(bill_state)*100).round
-end
-
-  
-def full_shipping_name
-  ship_first_name + " " + ship_last_name
-end
-  
-def full_billing_name
-  bill_first_name + " " + bill_last_name
-end
-  
-def full_shipping_street
-  ship_street_1 + " " + ship_street_2
-end
-  
-def full_billing_street
-  bill_street_1 + " " + bill_street_2
-end
-  
-  
-def total_price
-  order_items.sum(:price)
-end
-  
-def full_tax
-  if bill_state == "NJ" then
-    return sales_tax
-  else
-    return 0
+    response.success?
   end
-end
   
-  
-def grand_total_price
-  (total_price || 0) + (full_tax || 0) + (shipping_cost || 0) - (coupon_value || 0) - (store_wide_sale || 0)
-end
+  def price_in_cents(cart)
+    (cart.grand_total_price(bill_state)*100).round
+  end
 
-def calc_percent_store_wide_sale 
-  (store_wide_sale*100)/total_price rescue 0
-end
   
-private
+  def full_shipping_name
+    ship_first_name + " " + ship_last_name
+  end
   
-def purchase_options
-  {
-    ip: ip_address,
-    order_id: id,
-    email: user.name,
-      billing_address: {
-      name: bill_first_name + " " + bill_last_name,
-      address1: bill_street_1 + " " + bill_street_2,
-      city: bill_city,
-      state: bill_state,
-      country: "US",
-      zip: bill_zip
-    },
-      shipping_address: {
-      name: ship_first_name + " " + ship_last_name,
-      address1: ship_street_1 + " " + ship_street_2,
-      city: ship_city,
-      state: ship_state,
-      country: "US",
-      zip: ship_zip
-    }
-  }
-end
+  def full_billing_name
+    bill_first_name + " " + bill_last_name
+  end
   
-def validate_card
-  unless credit_card.valid?
-    credit_card.errors.full_messages.each do |message|
-      errors.add_to_base message
+  def full_shipping_street
+    ship_street_1 + " " + ship_street_2
+  end
+  
+  def full_billing_street
+    bill_street_1 + " " + bill_street_2
+  end
+  
+  
+  def total_price
+    order_items.sum(:price)
+  end
+  
+  def full_tax
+    if bill_state == "NJ" then
+      return sales_tax
+    else
+      return 0
     end
   end
-end
   
-def credit_card
-  @credit_card ||= ActiveMerchant::Billing::CreditCard.new(
-    type: credit_card_type,
+  
+  def grand_total_price
+    (total_price || 0) + (full_tax || 0) + (shipping_cost || 0) - (coupon_value || 0) - (store_wide_sale || 0)
+  end
+
+  def calc_percent_store_wide_sale 
+    (store_wide_sale*100)/total_price rescue 0
+  end
+  
+
+
+  private
+  
+  def purchase_options
+    {
+      ip: ip_address,
+      order_id: id,
+      email: user.name,
+      billing_address: {
+        name: bill_first_name + " " + bill_last_name,
+        address1: bill_street_1 + " " + bill_street_2,
+        city: bill_city,
+        state: bill_state,
+        country: "US",
+        zip: bill_zip
+      },
+      shipping_address: {
+        name: ship_first_name + " " + ship_last_name,
+        address1: ship_street_1 + " " + ship_street_2,
+        city: ship_city,
+        state: ship_state,
+        country: "US",
+        zip: ship_zip
+      }
+    }
+  end
+  
+
+  
+  def credit_card
+    @credit_card ||= ActiveMerchant::Billing::CreditCard.new(
+      type: credit_card_type,
       number: cc_number,
       verification_value: cc_verification,
       month: cc_expires.month,
       year: cc_expires.year,
       first_name: bill_first_name,
       last_name: bill_last_name
-  )
-end
+    )
+  end
 
 
 
