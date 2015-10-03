d8afcc70be770b97cdf2ec860e477a2a362422f1
Robert Lee Little III <rob@silverwebsystems.com>
2015-10-03 14:09:24 -0400

    added require in gem engine.

--- lib/silverweb_ecom/engine.rb
+++ lib/silverweb_ecom/engine.rb
@@ -2,7 +2,8 @@ module SilverwebEcom
   class Engine < ::Rails::Engine
         require 'geocoder'
         require 'ssl_requirement'
-
+        require 'bartt-ssl_requirement'
+        
   # load all local migrations
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
