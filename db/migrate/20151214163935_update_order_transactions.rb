32c820ba1adf31aa1b601a8f6ba89964fbe28986
Robert Lee Little III <rob@silverwebsystems.com>
2015-12-14 11:43:09 -0500

    Changed message column to be a text column to support longer messages.

---
+++ db/migrate/20151214163935_update_order_transactions.rb
@@ -0,0 +1,8 @@
+class UpdateOrderTransactions < ActiveRecord::Migration
+  def up
+    change_column :order_transactions, :message, :text
+  end
+  def down
+    change_column :order_transactions, :message, :string
+  end
+end
