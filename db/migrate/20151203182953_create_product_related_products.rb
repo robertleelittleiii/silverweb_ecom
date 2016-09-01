e65a49c70a12f27686521ecd907938a94bab8df5
Robert Lee Little III <rob@silverwebsystems.com>
2016-09-01 15:07:50 -0400

    added checks for table creation.

--- db/migrate/20151203182953_create_product_related_products.rb
+++ db/migrate/20151203182953_create_product_related_products.rb
@@ -1,10 +1,12 @@
 class CreateProductRelatedProducts < ActiveRecord::Migration
   def change
-    create_table :product_related_products do |t|
-      t.integer :product_id
-      t.integer :related_product_id
+    if not ActiveRecord::Base.connection.table_exists? 'product_related_products' then
+      create_table :product_related_products do |t|
+        t.integer :product_id
+        t.integer :related_product_id
 
-      t.timestamps null: false
+        t.timestamps null: false
+      end
     end
   end
 end
